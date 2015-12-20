406e9d503688da849fc721134231f1830cef106b
Thomas Stratmann <thomas.stratmann@9elements.com>
2015-12-20 12:12:21 +0100

    change versions only when not already set; print some output when doing so

--- xvm.source.bash
+++ xvm.source.bash
@@ -4,6 +4,13 @@ function xvm_chruby {
   version=
   if { read -r version < .ruby-version; } 2>/dev/null || [[ -n "$version" ]]; then
     version="${version%%[[:space:]]}"
+
+    if [ "$version" == "$XVM_CHRUBY" ]; then
+      return 0
+    fi
+    XVM_CHRUBY="$version"
+
+    echo "xvm: calling chruby $version"
     chruby "$version"
   fi
 }
@@ -13,6 +20,12 @@ function xvm_evm {
 
   version=
   if { read -r version < .xvm-evm; } 2>/dev/null || [[ -n "$version" ]]; then
+    if [ "$version" == "$XVM_EVM" ]; then
+      return 0
+    fi
+    XVM_EVM="$version"
+
+    echo "xvm: calling evm use $version"
     evm use "$version"
   fi
 }
@@ -22,6 +35,12 @@ function xvm_kiex {
 
   version=
   if { read -r version < .xvm-kiex; } 2>/dev/null || [[ -n "$version" ]]; then
+    if [ "$version" == "$XVM_KIEX" ]; then
+      return 0
+    fi
+    XVM_KIEX="$version"
+
+    echo "xvm: calling kiex use $version"
     kiex use "$version"
   fi
 }
